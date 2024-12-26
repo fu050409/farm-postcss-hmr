24f48ace1daa958c756f4e197bd0650d3d1d35eb
苏向夜 <fu050409@163.com>
2024-12-26 20:41:33 +0800

    fix: fixed in https://github.com/farm-fe/farm/pull/2027

--- src/views/page.vue
+++ src/views/page.vue
@@ -1,6 +1,6 @@
 <template>
-    <!-- Consider change the `bg-gray-800` to `bg-gray-900` -->
-    <div class="w-full h-full bg-gray-800 gap-2">
+    <!-- Consider change the `bg-gray-800` to `bg-blue-900` -->
+    <div class="w-full h-full bg-cyan-500">
 
     </div>
 </template>
